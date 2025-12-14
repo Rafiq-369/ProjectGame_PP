9b1828757a04dceace834bdf5d2b390a394bf4d6
Rafiq <rafiq43934l@gmail.com>
2025-12-14 10:02:34 +0700

    update map labirin

--- main.cpp
+++ main.cpp
@@ -1,44 +1,32 @@
-#include <ncurses/curses.h>
-#include <vector>
+#include <ncurses/curses.h>   // Library UI terminal
+#include <vector>             // Untuk vector
+#include <string>             // Untuk string
+#include <map>                // Untuk menyimpan box dengan nama
 
+using namespace std;
+
+// ===============================
+// MAP DASAR GAME
+// '#' = tembok
+// 'P' = player
+// 'A', 'B' = box
+// 'X', 'Y' = goal
+// ===============================
 vector<string> baseMap = {
     "####################",
-    "#     #       X    #",
-    "# ### # ## #### ####",
-    "#   A      #       #",
-    "# #   #### ##  ######",
-    "# #        #        #",
-    "# ## ###   #  ####  #",
-    "#     B    #     P  #",
+    "#     #          X #",
+    "# ### # ####### ####",
+    "# #  A             #",
+    "# #   #######   ####",
+    "#     B            #",
+    "# #######  ### ### #",
+    "#          # Y   P #",
     "####################"
 };
 
-int main() {
-  initscr();
-  noecho();
-
-   while (true) {
-        clear();
-
-        auto displayMap = updateMap(baseMap, player, boxes);
-
-        for (auto& row : displayMap)
-            printw("%s\n", row.c_str());
-
-        printw("\nGerak: W A S D | Q untuk keluar\n");
-
-        int ch = getch();
-        if (ch == 'q' || ch == 'Q') break;
-
-        int dr = 0, dc = 0;
-        if (ch == 'w') dr = -1;
-        if (ch == 's') dr = 1;
-        if (ch == 'a') dc = -1;
-        if (ch == 'd') dc = 1;
-
-        int nr = player.r + dr;
-        int nc = player.c + dc;
-     
-  endwin();
-return 0;
-}
+// ===============================
+// STRUKTUR POSISI (BARIS & KOLOM)
+// ===============================
+struct Pos {
+    int r, c;
+};
